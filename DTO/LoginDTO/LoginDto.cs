b274cf79b0d37e0aa3db5d1f89d8decac13bff93
David-ux-gif <invictus4044@gmail.com>
2025-03-05 20:01:37 +0545

    backkk

--- DTO/LoginDTO/LoginDto.cs
+++ DTO/LoginDTO/LoginDto.cs
@@ -7,7 +7,7 @@ namespace Backend.DTO.LoginDTO
 {
     public class LoginDto
     {
-        public string Username { get; set; } = String.Empty;
+        public string Username { get; set; } = String.Empty ;
 
         public string Password { get; set; } = String.Empty;
     }
