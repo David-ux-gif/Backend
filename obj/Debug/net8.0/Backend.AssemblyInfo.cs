0a5fcbcf30a364908eab0c8f24c85c9d2233ae1e
David-ux-gif <invictus4044@gmail.com>
2025-03-23 22:35:28 +0545

    ddd

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326cf88a51ef383f5fc28c839104670415d02ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b274cf79b0d37e0aa3db5d1f89d8decac13bff93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
